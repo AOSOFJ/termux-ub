9423130a271f1789e0c829084d508e6a4dbb8303
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-07 17:20:44 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -217,6 +217,15 @@ async def spam(event):
 async def _restart(event):
   await event.edit("**OK**")
   os.system("python termux-ub")
-  
+ 
+group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON)
+group_call = group_call_factory.get_file_group_call('input')
+
+@user.on(events.NewMessage(outgoing=True, pattern=r'^/join$'))
+async def join_handler(event):
+    chat = await event.get_chat()
+    await group_call.start(chat.id)
+
+app.run_until_disconnected()
 
 user.run_until_disconnected()
