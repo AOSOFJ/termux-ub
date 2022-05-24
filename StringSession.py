58952a2113df0395db14d023e5ef1db207c54d77
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-05-24 14:35:11 +0530

    Update StringSession.py

--- StringSession.py
+++ StringSession.py
@@ -1,1 +1,8 @@
-from telethon.Session import sessions
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+
+API_ID = input("Enter API_ID: ")
+API_HASH = input("Enter API_HASH: ")
+
+with TelegramClient(StringSession(),API_ID,API_HASH) as termuxUb:
+  termuxUb.send_message("me",f"**This Is You Session:-**\n\n{termuxUb.session.save()}")
